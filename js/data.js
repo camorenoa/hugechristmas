eac063c59e1b19954cf29f319138fc9c9cc354f7
gonzaloramirez <gonzaloramirez@google.com>
2018-12-09 19:26:47 -0500

    firebase

---
+++ js/data.js
@@ -0,0 +1,58 @@
+class HugeData {
+  constructor() {
+    this.data = {};
+    this.init();
+    this.post = {
+      name: '', 
+      purpose: '', 
+      office: ''
+    };
+  }
+
+  init() {
+    this.connectToFirebase();
+  }
+
+  connectToFirebase() {
+    var config = {
+      apiKey: "AIzaSyBeUzMx48ndfoavV0NO5Ip2a_DdvdnE_4k",
+      authDomain: "projectId.firebaseapp.com",
+      databaseURL: "https://christmas-huge.firebaseio.com/",
+      storageBucket: "bucket.appspot.com"
+    };
+    firebase.initializeApp(config);
+    this.data = firebase.database();
+  }
+
+  writeData(post) {
+    const newPostKey = firebase.database().ref().child('purposes').push().key;
+    let updates = {};
+    let response = undefined;
+    updates['purposes/' + newPostKey] = post;
+    response = firebase.database().ref().update(updates);
+
+    response.then((data) => {
+      let event = new Event("closeForm", {bubbles: true});
+      document.dispatchEvent(event);
+    });
+    return true;
+  }
+
+  readData() {
+    let posts = firebase.database().ref('purposes/');
+    let response = [];
+    
+    posts.orderByKey().on('child_added', function(snapshot) {
+      response.push({
+        name: snapshot.val().name,
+        office: snapshot.val().office,
+        purpose: snapshot.val().purpose
+      });
+    });
+    
+    return response;
+  }
+  
+};
+
+export default HugeData; 
